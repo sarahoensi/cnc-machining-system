c5718c71ae37f36b5fad608bc90c1dcf67c2800b
Sara <sarahoensi@outlook.com>
2026-02-09 10:20:03 +0100

    Update chip_load.rs

--- src-tauri/src/domain/machining_physics/calculators/chip_load.rs
+++ src-tauri/src/domain/machining_physics/calculators/chip_load.rs
@@ -24,21 +24,3 @@ impl ChipLoadCalculator {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::domain::units::machining::ChipLoad;
-    use crate::domain::units::motion::{FeedRate, Rpm};
-
-    #[test]
-    fn chipload_roundtrip() {
-        let chip = ChipLoad::mm_per_tooth(0.05).unwrap();
-        let rpm = Rpm::new(1000.0).unwrap();
-        let teeth = ToothCount::new(4).unwrap();
-
-        let feed = FeedRate::mm_per_min(chip.mm_per_tooth_value() * rpm.value() * 4.0).unwrap();
-        let chip2 = ChipLoadCalculator::chip_load_from_feed_rate(feed, rpm, teeth).unwrap();
-
-        assert!((chip2.mm_per_tooth_value() - 0.05).abs() < 1e-9);
-    }
-}
