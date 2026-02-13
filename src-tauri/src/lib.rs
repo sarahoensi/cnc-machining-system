e93609ef44354de00e277dfc2a4398d57e84d7cf
Sara <sarahoensi@outlook.com>
2026-02-13 11:36:57 +0100

    Update lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,21 +4,30 @@
 
 pub mod application;
 pub mod domain;
+pub mod infrastructure;
 pub mod interface;
 pub mod test_utils;
-pub mod infrastructure;
 
-use crate::interface::{solve_cutting_data, solve_helix, solve_right_triangle, 
-    generate_finishing_plan, register_finishing_measurement};
+use crate::interface::{
+    generate_finishing_plan, register_finishing_measurement, solve_cutting_data, solve_helix,
+    solve_right_triangle,
+};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
+            // right triangle
             solve_right_triangle,
+
+            // helix
             solve_helix,
+
+            // cutting_data
             solve_cutting_data,
+
+            // finishing
             generate_finishing_plan,
             register_finishing_measurement,
         ])
