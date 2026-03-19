b7edc492ea343173cb705fd4252f431bfb01caa4
Sara <sarahoensi@outlook.com>
2026-03-19 10:29:18 +0100

    validator

---
+++ src/shared/form/types/validate.ts
@@ -0,0 +1,8 @@
+// shared/form/types/validate.ts
+
+import type { FieldState } from "@shared/form/types/fields";
+
+export type FormValidateFn<K extends string, E> = (
+  fields: Record<K, FieldState>,
+  extras: E
+) => string | null;
